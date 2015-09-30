d8f36673cff73c0f0871a8267d274ce99c2c0bb7
alejo80805 <alejo80805@hotmail.com>
2015-09-29 21:43:41 -0500

    Creando la clase DBHelper

---
+++ app/src/main/java/arf/com/everpobre/model/db/DBHelper.java
@@ -0,0 +1,23 @@
+package arf.com.everpobre.model.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
