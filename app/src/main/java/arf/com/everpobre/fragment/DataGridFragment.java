a09fa2f539d0ab3639605c0f617db4c158385c8a
Alejandro Rodriguez <alejo80805@hotmail.com>
2015-10-04 11:52:05 -0500

    se implemento el onItemListener

--- app/src/main/java/arf/com/everpobre/fragment/DataGridFragment.java
+++ app/src/main/java/arf/com/everpobre/fragment/DataGridFragment.java
@@ -1,15 +1,19 @@
 package arf.com.everpobre.fragment;
 
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import arf.com.everpobre.R;
+import arf.com.everpobre.activities.EditNotebookActivity;
 import arf.com.everpobre.adapter.DataGridAdapter;
 import arf.com.everpobre.model.dao.NotebookDAO;
 
@@ -43,5 +47,29 @@ public class DataGridFragment extends Fragment {
         cursor = new NotebookDAO(getActivity()).queryCursor();
         adapter = new DataGridAdapter(getActivity(),cursor);
         gridView.setAdapter(adapter);
+
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), "se hizo click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getActivity(), "se hizo long click", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity(), EditNotebookActivity.class);
+                startActivity(intent);
+
+
+                /*Si se pone true quiere decir que yo ya hice el handle del onClick
+                por tal razón no pasa al onItemClick*/
+                return true;
+            }
+        });
     }
 }
